2df6b85cb4a6878fe2162274be0cf63c4ea40601
Jesse B. Miller <jesse@jessebmiller.com>
2019-09-07 16:51:45 -0400

    demo uis

---
+++ demoUIs/bootstrap/index.js
@@ -0,0 +1,234 @@
+const Http = new XMLHttpRequest()
+
+[
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "proposedUi",
+				"type": "string"
+			}
+		],
+		"name": "execute",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "join",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "proposedUi",
+				"type": "string"
+			}
+		],
+		"name": "propose",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "newUi",
+				"type": "string"
+			}
+		],
+		"name": "set",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "proposedUi",
+				"type": "string"
+			},
+			{
+				"internalType": "bool",
+				"name": "yay",
+				"type": "bool"
+			}
+		],
+		"name": "vote",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "holdings",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "maxHoldings",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "nays",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "proposals",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "ui",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "bytes32",
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"name": "voted",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "votingPeriodBlocks",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "yays",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
--- voting-ui/src/App.js
+++ voting-ui/src/App.js
@@ -4,48 +4,39 @@ import Web3 from 'web3';
 
 import NaiveStateDAO from './NaiveStateDAO.json'
 
+const daoAddress = "0xDD0E544Bc37A58423CEE63E67d05e7d978DEE6c7"
+
+let web3
+let accounts
+let options
+
 async function addWeb3() {
   // Modern dapp browsers...
   if (window.ethereum) {
-      window.web3 = new Web3(ethereum)
-      try {
-          // Request account access if needed
-          await ethereum.enable();
-      } catch (error) {
-          // User denied account access...
-          console.log(error)
-      }
+    web3 = new Web3(window.ethereum)
+    try {
+      // Request account access if needed
+      await window.ethereum.enable();
+    } catch (error) {
+      // User denied account access...
+      console.log(error)
+    }
   }
   // Legacy dapp browsers...
   else if (window.web3) {
-      window.web3 = new Web3(web3.currentProvider)
+    web3 = new Web3(window.web3.currentProvider)
   }
   // Non-dapp browsers...
   else {
-      console.log('Minting, buying, or selling tokens requires a connection to Ethereum. \
+    console.log('Minting, buying, or selling tokens requires a connection to Ethereum. \
                    Consider installing/logging into the Metamask browser extension.')
   }
 }
 
-let web3
-let accounts
-let stateDAO
-let options
-
 async function init(defaults) {
-  if (web3 !== undefined) { return }
-
-  // Development env (non-browser)
-  if (process.env.DEVELOPMENT === 'true') {
-    // add ganache web3
-    const provider = new Web3.providers.WebsocketProvider('ws://127.0.0.1:8545')
-    web3 = new Web3(provider)
-  } else {
-    await addWeb3()
-    web3 = window.web3
-  }
-
+  await addWeb3()
   accounts = await web3.eth.getAccounts()
+  console.log("accounts", accounts)
 
   if (defaults === undefined) {
     defaults = {
@@ -54,19 +45,26 @@ async function init(defaults) {
     }
   }
   options = defaults
-  console.log("init", options)
-  stateDAO = await new web3.eth.Contract(
+  console.log(daoAddress)
+  const stateDAO = await new web3.eth.Contract(
     NaiveStateDAO.abi,
-    process.env.STATE_DAO_ADDR,
+    daoAddress,
     defaults
   )
+  console.log("init", options, stateDAO)
+  return stateDAO
 }
 
 function App() {
-  init()
-  console.log(stateDAO)
+  let currentUi = "loading current ui"
+  init().then(async (dao) => {
+    console.log(dao)
+    currentUi = await dao.methods.ui().call()
+    console.log(currentUi)
+  })
   return (
     <div className="App">
+
     </div>
   );
 }
